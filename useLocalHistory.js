4b8116037f7e0465f9e1f43bf8dd203b059475d6
Jonathan Dorety <jdorety@flur.ee>
2021-01-22 09:43:23 -0500

    Add history hook

---
+++ useLocalHistory.js
@@ -0,0 +1,22 @@
+// import { useState, useEffect } from "react";
+
+/**
+ * Provides a state variable consisting of an array of objects
+ * Will not add consecutive duplicate items
+ * @param {string} storageKey 
+ * @returns {[Array, (value: object) => void]}
+ */
+function useLocalHistory(storageKey) {
+  const storedState = localStorage.getItem(storageKey);
+  const [state, setState] = useState(
+    storedState ? JSON.parse(storedState) : []
+  );
+
+  useEffect(() => {
+    const storedData = localStorage.getItem(storageKey);
+
+    if (storedData !== JSON.stringify(state))
+      localStorage.setItem(storageKey, JSON.stringify(state));
+  }, [state]);
+  return [state, setState];
+}
